7d382fb181ea5a2338b7c930f4fae85d24b28874
shaonghosh <shaonghosh@gmail.com>
2017-04-24 17:31:31 -0500

    Update rankedTilesGenerator.py
    Added functionality for adaptive integration time and optimization of the adaptation.

--- rankedTilesGenerator.py
+++ rankedTilesGenerator.py
@@ -18,6 +18,8 @@ import datetime
 from astropy.time import Time
 from astropy import units as u
 from astropy.coordinates import get_sun
+from astropy.coordinates import get_moon
+from astropy.coordinates import get_body
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 
 # from AllSkyMap_basic import AllSkyMap
@@ -226,28 +228,87 @@ class RankedTileGenerator:
 		
 		return [ranked_galaxies, galaxy_probs]
 
-	def integrationTime(self, T_obs, pValTiles=None):
+	### Older version ###	
+
+# 	def integrationTime(self, T_obs, pValTiles=None):
+# 		'''
+# 		METHOD :: This method accepts the probability values of the ranked tiles, the 
+# 			  total observation time and the rank of the source tile. It returns 
+# 			  the array of time to be spent in each tile which is determined based
+# 			  on the localizaton probability of the tile. 
+# 				  
+# 		pValTiles :: The probability value of the ranked tiles. Obtained from ZTF_RT 
+# 					 output
+# 		T_obs     :: Total observation time available for the follow-up.
+# 		'''
+# 		if pValTiles is None:
+# 			pValTiles = self.allTiles_probs_sorted
+# 		
+# 		fpValTiles = pValTiles ### If we need to modify the probability weight.
+# 		modified_prob = fpValTiles/np.sum(fpValTiles)
+# 		t_tiles = modified_prob * T_obs ### Time spent in each tile if not constrained
+# 		t_tiles[t_tiles > 1200.0] = 1200.0 ### Upper limit of exposure time
+# 		t_tiles[t_tiles < 60] = 60.0 ### Lower limit of exposure time
+# 		Obs = np.cumsum(t_tiles) <= T_obs ### Tiles observable in T_obs seconds
+# 		time_per_tile = t_tiles[Obs] ### Actual time spent per tile
+# 		
+# 		return time_per_tile
+		
+
+
+
+	def integrationTime(self, T_obs, pValTiles=None, func=None):
 		'''
 		METHOD :: This method accepts the probability values of the ranked tiles, the 
 			  total observation time and the rank of the source tile. It returns 
 			  the array of time to be spent in each tile which is determined based
-			  on the localizaton probability of the tile. 
+			  on the localizaton probability of the tile. How the weight factor is 
+			  computed can also be supplied in functional form. Default is linear.
 				  
 		pValTiles :: The probability value of the ranked tiles. Obtained from ZTF_RT 
 					 output
 		T_obs     :: Total observation time available for the follow-up.
+		func	  :: functional form of the weight. Default is linear. 
+					 For example, use x**2 to use a quadratic function.
 		'''
 		if pValTiles is None:
 			pValTiles = self.allTiles_probs_sorted
-		t_tiles = pValTiles * T_obs ### Time spent in each tile if not constrained
+		
+		if func is None:
+			f = lambda x: x
+		else:
+			f = lambda x: eval(func)
+		fpValTiles = f(pValTiles)
+		modified_prob = fpValTiles/np.sum(fpValTiles)
+		t_tiles = modified_prob * T_obs ### Time spent in each tile if not constrained
 		t_tiles[t_tiles > 1200.0] = 1200.0 ### Upper limit of exposure time
 		t_tiles[t_tiles < 60] = 60.0 ### Lower limit of exposure time
 		Obs = np.cumsum(t_tiles) <= T_obs ### Tiles observable in T_obs seconds
 		time_per_tile = t_tiles[Obs] ### Actual time spent per tile
 		
 		return time_per_tile
-	
-	
+		
+
+	def optimize_time(self, T, M, range, pValTiles=None):
+		time_data, limmag_data, _ = np.loadtxt('timeMagnitude_new.dat', unpack=True)
+		s = interpolate.UnivariateSpline(np.log(time_data), limmag_data, k=5)
+		AA = np.linspace(range[0], range[1], 1000)
+		if pValTiles is None:
+			pValTiles = self.allTiles_probs_sorted
+		kappa_sum = []		
+		for aa in AA:
+			time_per_tile = self.integrationTime(T, pValTiles, func='x + ' + str(aa))
+			limmag = s(np.log(time_per_tile))
+			dists = 10**(1.0 + (limmag - M)/5)
+			kappas = dists*pValTiles[:len(dists)]
+			kappa_sum.append(np.sum(kappas))
+		kappa_sum = np.array(kappa_sum)
+		maxIndex = np.argmax(kappa_sum)
+		a_max = AA[maxIndex]
+		kappa_max = kappa_sum[maxIndex]
+		time_per_tile_max = self.integrationTime(7200., pValTiles, func='x + ' + str(a_max))
+		return time_per_tile_max
+		
 ############ UNDER CONSTRUCTION ############
 	
 class Scheduler(RankedTileGenerator):
@@ -265,17 +326,17 @@ class Scheduler(RankedTileGenerator):
 				 tileCoord='ZTF_tiles_set1_nowrap_indexed.dat', utcoffset = -7.0):
 
 		self.Observatory = EarthLocation.of_site(site)
-		tileData = np.recfromtxt(tileCoord, names=True)
+		self.tileData = np.recfromtxt(tileCoord, names=True)
 		self.skymapfile = skymapFile
 		
 		tileObj = RankedTileGenerator(skymapFile)
 		[self.tileIndices, self.tileProbs] = tileObj.ZTF_RT()
 
-		self.tiles = SkyCoord(ra = tileData['ra_center'][self.tileIndices]*u.degree, 
-					    dec = tileData['dec_center'][self.tileIndices]*u.degree, 
+		self.tiles = SkyCoord(ra = self.tileData['ra_center'][self.tileIndices]*u.degree, 
+					    dec = self.tileData['dec_center'][self.tileIndices]*u.degree, 
 					    frame = 'icrs') ### Tile(s)
 		self.utcoffset = utcoffset*u.hour
-		
+
 
 	def tileVisibility(self, t, gps=False):
 		'''
@@ -297,7 +358,52 @@ class Scheduler(RankedTileGenerator):
 # 		return [altAz_tile, self.tileProbs, altAz_sun]
 		return [self.tileIndices[whichTilesUp], self.tileProbs[whichTilesUp], altAz_sun]
 		
+
+	def advanceToSunset(self, eventTime, intTime):
+		'''
+		This method is called when the observation scheduler determines that the sun is 
+		above horizon. It finds the nearest time prior to the next sunset within +/- 
+		integration time and then advances the scheduler code to that point. 
+		This speeds up the code by refraning from computing pointings during the daytime.
+		Currently only works with GPS time. In the future mjd will also be included.
 		
+		eventTime	:: The GPS time for which the advancement is to be computed.
+		intTim		:: The integration time for the obsevation
+		'''
+		
+		dt = np.arange(0, 24*3600 + intTime, intTime)
+		time = Time(eventTime + dt, format='gps')
+		altAz_sun = get_sun(time).transform_to(AltAz(obstime=time, location=self.Observatory))
+		timeBeforeSunset = (eventTime + dt)[altAz_sun.alt.value < -18.0][0] - intTime
+		return timeBeforeSunset
+
+
+	############### NOT TESTED ############
+	def whenThisTileSets(self, index, currentTime, duration, gps=False):
+		'''
+		This method approximately computes the amount of time left in seconds for a tile
+		to set below 20 degrees.
+		
+		index		::	The index of the tile for which setting tile is to be found
+		currentTime	::	The current time when this tile is scheduled
+		'''
+# 		if gps: time = Time(currentTime, format='gps')
+# 		else: time = Time(currentTime, format='mjd')
+		thisTile = SkyCoord(ra = self.tileData['ra_center'][index]*u.degree, 
+					    dec = self.tileData['dec_center'][index]*u.degree, 
+					    frame = 'icrs') ### Tile(s)
+		dt = np.arange(0, duration + 1.0, 1.0)
+		times = Time(currentTime + dt, format='gps')
+		altAz_tile = thisTile.transform_to(AltAz(obstime=times,
+												location=self.Observatory))
+		
+		setTime = None
+		if altAz_tile.alt.value[-1] < 20.0:
+			s = interpolate.UnivariateSpline(altAz_tile.alt.value, times.value, k=3)
+			setTime = s(20.0)
+			
+		return setTime
+
 	def observationSchedule(self, duration, eventTime, integrationTime=120,
 							observedTiles=None, plot=False, verbose=False):
 		'''
@@ -309,54 +415,170 @@ class Scheduler(RankedTileGenerator):
 		eventTime  		 :: The gps time of the time of the GW trigger.
 		integrationTime  :: Time spent per tile in seconds (default == 120 seconds)
 		observedTiles	 :: (Future development) Array of tile indices that has been 
-							observed in n earlier epoch
+							observed in an earlier epoch
 		plot			 :: (optional) Plots the tile centers that are observed.
 		verbose			 :: Toggle verbose flag for print statements.
 				   
 		
 		'''
 		
-		if plot:
-			from AllSkyMap_basic import AllSkyMap
-			
-						
+		includeTiles = np.cumsum(self.tileProbs) < 0.99
+		includeTiles[np.sum(includeTiles)] = True
+		
+		thresholdTileProb = self.tileProbs[includeTiles][-1]
+
+
 
 		observedTime = 0 ## Initiating the observed times
+		elapsedTime = 0  ## Initiating the elapsed times. Time since observation begun.
 		scheduled = np.array([]) ## tile indices scheduled for observation
 		ObsTimes = []
 		pVal_observed = []
 		ii = 0
 		observed_count = 0
-		while observedTime <= duration:
-			dt = integrationTime * ii
-			[tileIndices, tileProbs, altAz_sun] = self.tileVisibility(eventTime + dt,
-																		 gps=True)
-			localTime = Time(eventTime + dt, format='gps') + self.utcoffset
+		sun_ra = []
+		sun_dec = []
+		moon_ra = []
+		moon_dec = []
+		venus_ra = []
+		venus_dec = []
+		lunar_ilumination = []
+		
+		
+		
+		[_, _, altAz_sun] = self.tileVisibility(eventTime, gps=True)
+		
+		if altAz_sun.alt.value >= -18.0:
+			if verbose: 
+				localTime = Time(eventTime, format='gps') + self.utcoffset
+				print str(localTime.utc.datetime) + ': Sun above the horizon'
+			eventTime = self.advanceToSunset(eventTime, integrationTime)
+			if verbose:
+				localTime = Time(eventTime, format='gps') + self.utcoffset
+				print 'Advancing time to ' + str(localTime.utc.datetime)
+				print '\n'
+
+		
+		while elapsedTime <= duration: 
+			[tileIndices, tileProbs, altAz_sun] = self.tileVisibility(eventTime, gps=True)
+			localTime = Time(eventTime, format='gps') + self.utcoffset
 			
-			if altAz_sun.alt.value < -18.0: ### Sun below horizon
-				observed_count += 1
-				observedTime += integrationTime ### Augment observed time
+			if altAz_sun.alt.value < -18.0: 
 				if verbose: 
 					print str(localTime.utc.datetime) + ': Observation mode'
-
 				for jj in np.arange(len(tileIndices)):
 					if tileIndices[jj] not in scheduled:
-					#if ~np.in1d(tileIndices[jj], scheduled):
-						scheduled = np.append(scheduled, tileIndices[jj])
-						ObsTimes.append(localTime)
-						pVal_observed.append(tileProbs[jj])
-						break
-
+						if tileProbs[jj] > thresholdTileProb:
+							scheduled = np.append(scheduled, tileIndices[jj])
+							ObsTimes.append(localTime)
+							pVal_observed.append(tileProbs[jj])
+							Sun = get_sun(Time(eventTime, format='gps'))
+							sun_ra.append(Sun.ra.value)
+							sun_dec.append(Sun.dec.value)
+							Moon = get_moon(Time(eventTime, format='gps'))
+							sunMoonAngle = Sun.separation(Moon)
+							phaseAngle = np.arctan2(Sun.distance*np.sin(sunMoonAngle), 
+										Moon.distance - Sun.distance *
+										np.cos(sunMoonAngle))
+							illumination = 0.5*(1.0 + np.cos(phaseAngle))
+							
+							if verbose: print 'Lunar illumination = ' + str(illumination)
+							lunar_ilumination.append(illumination)
+							
+							moon_ra.append(Moon.ra.value)
+							moon_dec.append(Moon.dec.value)
+							observedTime += integrationTime ## Tracking observations
+							break
+				
 			else:
-				if verbose: print str(localTime.utc.datetime) + ': Sun above the horizon'
+				if verbose: 
+					localTime = Time(eventTime, format='gps') + self.utcoffset
+					print str(localTime.utc.datetime) + ': Sun above the horizon'
+				eventTime = self.advanceToSunset(eventTime, integrationTime)
+				if verbose:
+					localTime = Time(eventTime, format='gps') + self.utcoffset
+					print 'Advancing time to ' + str(localTime.utc.datetime)
+					print '\n'
 			
+
 			ii += 1
+
+			eventTime += integrationTime
+			elapsedTime += integrationTime
+			print 'elapsedTime --->' + str(elapsedTime)
+			print 'observedTime --->' + str(observedTime)
+
+
+	
+# 		while observedTime <= duration:
+# 		while elapsedTime <= duration: 
+# 			print 'Observed time = ' + str(observedTime/3600.)
+# 			dt = integrationTime * ii
+# 			elapsedTime += integrationTime
+# 			print '**** Elapsed time = ' + str(elapsedTime) + '****'
+# 			[tileIndices, tileProbs, altAz_sun] = self.tileVisibility(eventTime,
+# 																		 gps=True)
+# 			localTime = Time(eventTime, format='gps') + self.utcoffset
+# 			
+# 
+# 			
+# 			if altAz_sun.alt.value < -18.0: ### Sun below horizon
+# 				observed_count += 1
+# 				observedTime += integrationTime ### Augment observed time
+# 				if verbose: 
+# 					print str(localTime.utc.datetime) + ': Observation mode'
+# 
+# 				for jj in np.arange(len(tileIndices)):
+# 					if tileIndices[jj] not in scheduled:
+# 						if tileProbs[jj] > thresholdTileProb:
+# 							scheduled = np.append(scheduled, tileIndices[jj])
+# 							ObsTimes.append(localTime)
+# 							pVal_observed.append(tileProbs[jj])
+# 							Sun = get_sun(Time(eventTime, format='gps'))
+# 							sun_ra.append(Sun.ra.value)
+# 							sun_dec.append(Sun.dec.value)
+# 							Moon = get_moon(Time(eventTime, format='gps'))
+# 							sunMoonAngle = Sun.separation(Moon)
+# 							phaseAngle = np.arctan2(Sun.distance*np.sin(sunMoonAngle), 
+# 										Moon.distance - Sun.distance *
+# 										np.cos(sunMoonAngle))
+# 							illumination = 0.5*(1.0 + np.cos(phaseAngle))
+# 							
+# 							if verbose: print 'Lunar illumination = ' + str(illumination)
+# 							lunar_ilumination.append(illumination)
+# 							
+# 							moon_ra.append(Moon.ra.value)
+# 							moon_dec.append(Moon.dec.value)
+# 							break
+# 
+# 			else:
+# 				if verbose: 
+# 					localTime = Time(eventTime, format='gps') + self.utcoffset
+# 					print str(localTime.utc.datetime) + ': Sun above the horizon'
+# 				eventTime = self.advanceToSunset(eventTime, integrationTime)
+# 				if verbose:
+# 					localTime = Time(eventTime, format='gps') + self.utcoffset
+# 					print 'Advancing time to ' + str(localTime.utc.datetime)
+# 					print '\n'
+
+
+
+
 		for ii in np.arange(len(scheduled)):
 			print str(ObsTimes[ii].utc.datetime) + '\t' + str(int(scheduled[ii]))
 			
-		return [scheduled.astype('int'), pVal_observed]
+		pVal_observed = np.array(pVal_observed)
+		sun_ra = np.array(sun_ra)
+		sun_dec = np.array(sun_dec)
+		moon_ra = np.array(moon_ra)
+		moon_dec = np.array(moon_dec)
+		venus_ra = np.array(venus_ra)
+		venus_dec = np.array(venus_dec)
+		
+		return [scheduled.astype('int'), pVal_observed, sun_ra, 
+				sun_dec, moon_ra, moon_dec, lunar_ilumination]
 
-				
+
 
 
 
@@ -414,14 +636,18 @@ def detectability(rank, time_per_tile, total_observation_time, absolute_mag, sou
 	total_observation_time 	 :: The total observation time for the event
 	absolute_magnitude 		 :: The absolute magnitude of the source that 
 					is to be set by the model.
-	source_dist_parsec 		 :: distance to source in parsecs time_data, 
-					limmag_data, error_data :: The data which 
-					needs to be interpolated to give limiting 
-					magnitude as a function of time. error_data, 
-					if provided, will allow for a detection 
-					probability to be generated as output. If 
-					detectability(rank, time_per_tile, total_observation_time, absolute_mag, source_dist_parsec, time_data, limmag_data, error_data = None, verbose=False)error_data is not provided, a Boolean 
-					(True/False) for detection will be output. 
+	source_dist_parsec 		 :: distance to source in parsecs 
+	time_data, 
+	limmag_data, 
+	error_data 				 :: The data which needs to be interpolated to give 
+								limiting magnitude as a function of time. error_data, 
+								if provided, will allow for a detection probability 
+								to be generated as output. If detectability(rank,
+								time_per_tile, total_observation_time, absolute_mag,
+								source_dist_parsec, time_data, limmag_data, 
+								error_data = None, verbose=False)
+								error_data is not provided, a Boolean (True/False) for
+								detection will be output. 
 	'''
 	### Convert to numpy object if scalar supplied
 	if isinstance(time_per_tile, (np.ndarray,)) == False:
@@ -457,7 +683,7 @@ def detectability(rank, time_per_tile, total_observation_time, absolute_mag, sou
 		samples = 10**5
 		x = np.linspace(apparent_mag, very_large_number, samples, endpoint = True)
 		### If floats are passed to the function, return the answer rightaway
-		if isinstance(mu, (float, np.float, np.float64,)) == True and  isinstance(sigma, (float, np.float, np.float64,)) == True:
+		if isinstance(mu, (float, np.float, np.float64,)) == True and isinstance(sigma, (float, np.float, np.float64,)) == True:
 			y = gaussian_distribution_function(x, mu, sigma)
 			return np.trapz(y,x)
 		# If an array of time_per_tile had been passed
